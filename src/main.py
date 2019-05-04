5d1e9a3e18e2c422521f15a72672a7464dbf97b5
Scott Reu <sreu@meraki.com>
2019-05-04 13:13:35 -0700

    add recaptcha keys

--- src/main.py
+++ src/main.py
@@ -1,29 +1,34 @@
 from random import randint
 from time import strftime
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, RecaptchaField
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
+app.config['SECRET_KEY'] = 'e657d3949722c1c3473c5704fd025f1d'
+
+RECAPTCHA_PUBLIC_KEY = "6Ld9yaEUAAAAAPOZ_ivD9Mwk85YdCmMyjRI_RxKC"
+RECAPTCHA_PRIVATE_KEY = "6Ld9yaEUAAAAAM5-vlSEfi913hKlTlTj58GslEkq"
+
 
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
     surname = TextField('Surname:', validators=[validators.required()])
+    recaptcha = RecaptchaField()
 
 def get_time():
     time = strftime("%Y-%m-%dT%H:%M")
     return time
 
-def write_to_disk(name, surname, email):
-    data = open('file.log', 'a')
-    timestamp = get_time()
-    data.write('DateStamp={}, Name={}, Surname={}, Email={} \n'.format(timestamp, name, surname, email))
-    data.close()
+# def write_to_disk(name, surname, email):
+#     data = open('file.log', 'a')
+#     timestamp = get_time()
+#     data.write('DateStamp={}, Name={}, Surname={}, Email={} \n'.format(timestamp, name, surname, email))
+#     data.close()
 
 @app.route("/", methods=['GET', 'POST'])
-def interview():
+def schedule():
     form = ReusableForm(request.form)
 
     #print(form.errors)
@@ -34,7 +39,9 @@ def interview():
         password=request.form['password']
 
     if form.validate():
-        write_to_disk(name, surname, email)
+        #
+        # Do your work here
+        #
         flash('Hello: {} {}'.format(name, surname))
 
     else:
